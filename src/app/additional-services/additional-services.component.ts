a6664a4cf6bd703d76f2ffe42901d4a51547021e
Andjela24 <1andjela.ljubenkovic@gmail.com>
2023-02-08 23:55:48 +0100

    initial commit

---
+++ src/app/additional-services/additional-services.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-additional-services',
+  templateUrl: './additional-services.component.html',
+  styleUrls: ['./additional-services.component.css']
+})
+export class AdditionalServicesComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { AdditionalServicesComponent } from './additional-services/additional-services.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormComponent } from './form/form.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { PricesComponent } from './prices/prices.component';
+import { UserReviewsComponent } from './user-reviews/user-reviews.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    AdditionalServicesComponent,
+    ServicesComponent,
+    FooterComponent,
+    FormComponent,
+    GalleryComponent,
+    HomeComponent,
+    PricesComponent,
+    UserReviewsComponent,
+    VehiclesComponent
   ],
   imports: [
     BrowserModule,
