9cb7ce759d05f27858a0aa03721e1795f2d7b34f
andjela24 <1andjela.ljubenkovic@gmail.com>
2023-05-12 02:28:05 +0200

    finished about section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  faPhone = faPhone;
+
+  constructor() {}
+  /*
+  ngOnInit() {
+    this.callButton = document.querySelector('#call-button');
+    this.callButton.addEventListener('click', () => {
+      window.location.href = 'tel:+38160123456';
+    });
+  
+    */
+  /*
+  readonly callButton: HTMLElement | null =
+    document.getElementById('call-button');
+
+  ngOnInit() {
+    if (this.callButton) {
+      this.callButton.addEventListener('click', () => {
+        window.location.href = 'tel:+38160123456';
+      });
+    }
+  }
+  */
+  @ViewChild('callButton', { static: false }) callButton!: ElementRef;
+
+  onClick() {
+    window.location.href = 'tel:+38160123456';
+  }
+}
