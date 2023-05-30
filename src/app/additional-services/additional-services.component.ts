c9fd859a582f38302452beb6b8f755ce4a08bb48
AndjelaLjubenkovic <andjela.ljubenkovic@sistemi.rs>
2023-05-30 16:32:57 +0200

    improved css

--- src/app/additional-services/additional-services.component.ts
+++ src/app/additional-services/additional-services.component.ts
@@ -13,24 +13,5 @@ export interface AdditionalService {
   styleUrls: ['./additional-services.component.css'],
 })
 export class AdditionalServicesComponent {
-  additionalCards: AdditionalService[] = [
-    {
-      id: 1,
-      imgUrl: './assets/images/cards/montaza-img.jpg',
-      title: 'Montaža / Demontaža',
-      text: ' Montiramo i dempntiramo već postojeći ili tek kupljeni nameštaj profesionalnim alatima. *Ne montiramo kuhinje. ',
-    },
-    {
-      id: 2,
-      imgUrl: './assets/images/cards/zastita-img.jpg',
-      title: 'Zaštita nameštaja',
-      text: ' Profesionalna zaštita stvari sreč folijom ili puc-puc folijom. ',
-    },
-    {
-      id: 3,
-      imgUrl: './assets/images/cards/pakovanje-img.jpg',
-      title: 'Pakovanje',
-      text: ' Vršimo pakovanje u kartonske kutije različitih dimenzija ili džakove različitih veličina. ',
-    },
-  ];
+  
 }
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -6,42 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.css'],
 })
 export class ServicesComponent {
-  cards = [
-    {
-      id: 1,
-      imgUrl: './assets/images/cards/selidbe-img.webp',
-      title: 'Selidbe',
-      text: 'Vršimo selidbe lokala, zdraga, kuća i magacina. Selidba uključuje utovar, prevoz i sitovar. Isključivo sa našim radnicima. Može biti i na više lokacija.',
-    },
-    {
-      id: 2,
-      imgUrl: './assets/images/cards/prevoz-img.webp',
-      title: 'Prevoz',
-      text: 'Nudimo usluge prevoza stvari, nameštaja i ostale robe težine do 1.5t.',
-    },
-    {
-      id: 3,
-      imgUrl: './assets/images/cards/kurir-img.webp',
-      title: 'Usluge kurirske službe',
-      text: 'Vi poručujete, mi plaćamo za Vas i dostavljamo Vam na željenu adresu.',
-    },
-    {
-      id: 4,
-      imgUrl: './assets/images/cards/odnosenje-stvari-img.webp',
-      title: 'Odnošenje starih stvari',
-      text: 'Dolazimo na željenu adresu i odnosimo stare stvari na deponiju.',
-    },
-    {
-      id: 5,
-      imgUrl: './assets/images/cards/teske-stvari-img.webp',
-      title: 'Prevoz teških stvari',
-      text: 'Za Vas prevozimo teške i kabaste stvari kao što su klavie, sef i mnoge druge.',
-    },
-    {
-      id: 6,
-      imgUrl: './assets/images/cards/razmestaj-img.webp',
-      title: 'Razmestaj nameštaja',
-      text: 'Za Vas nudimo uslugu razmeštaja nameštaja u Vašem objektu.',
-    },
-  ];
+  
 }
