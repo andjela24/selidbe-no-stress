8686cb4c9e1ff9723a46fb2f9f35a15ce78ba383
AndjelaLjubenkovic <andjela.ljubenkovic@sistemi.rs>
2023-06-14 13:12:47 +0200

    added function to home button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , ElementRef, OnInit } from '@angular/core';
 import {
   faTruck,
   faClock,
@@ -10,8 +10,25 @@ import {
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   faTruck = faTruck;
   faClock = faClock;
   faPiggyBank = faPiggyBank;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {}
+
+  scrollTo(id: string) {
+    console.log(`scrolling to ${id}`);
+    let el = document.getElementById(id);
+    if (el) {
+      el.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
+  }
 }
+
